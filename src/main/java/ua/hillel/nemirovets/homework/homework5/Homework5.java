f28aabdb57a2ee1cbe3a527a92d19be5e18d969c
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-02-17 13:53:23 +0200

    homework5

---
+++ src/main/java/ua/hillel/nemirovets/homework/homework5/Homework5.java
@@ -0,0 +1,20 @@
+package ua.hillel.nemirovets.homework.homework5;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        int liWarrior = 13;
+        int liBowman = 24;
+        int liRider = 46;
+        int countLiWarrior = 860;
+        int liGeneralAttack  = (liWarrior + liBowman + liRider) * countLiWarrior;
+
+        int minWarrior = 9;
+        int minBowman = 15;
+        int minRider = 12;
+        double countMinWarrior = countLiWarrior* 1.5;
+        int minGeneralAttack = (minWarrior + minBowman + minRider) * (int) countMinWarrior;
+
+        System.out.println("General Attack Li = " + liGeneralAttack);
+        System.out.println("General Attack Min = " +minGeneralAttack);
+    }
+}
