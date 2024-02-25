fe216799a7ed240d7a9087c3f62d641f1662c2da
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-02-25 15:37:46 +0200

    add files Homework 7

--- src/main/java/ua/hillel/nemirovets/homework/homework7/Homework7.java
+++ src/main/java/ua/hillel/nemirovets/homework/homework7/Homework7.java
@@ -6,7 +6,7 @@ public class Homework7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int progValue = (int)(Math.random()*11);
+        int progValue = (int) (Math.random() * 11);
         System.out.println("The computer guessed an integer");
 
 
@@ -14,25 +14,25 @@ public class Homework7 {
         int counter = 6;
         System.out.println("Enter a number from 0 to 10");
 
-        while (counter >0){
-            if(scanner.hasNextInt()){
+        while (counter > 0) {
+            if (scanner.hasNextInt()) {
                 userValue = scanner.nextInt();
-                if (userValue < 0 || userValue > 10 ){
+                if (userValue < 0 || userValue > 10) {
                     System.out.println("You entered an incorrect number. The number must be between 0 and 10");
-                }else if(userValue == progValue){
+                } else if (userValue == progValue) {
                     System.out.println("Congratulation. You Win!!!");
                     break;
-                }else if(progValue > userValue){
+                } else if (progValue > userValue) {
                     System.out.println("Wrong data. Program value is bigger than " + userValue);
-                }else {
+                } else {
                     System.out.println("Wrong data. Program value is less than " + userValue);
                 }
-            }else{
+            } else {
                 System.out.println("Wrong data. The number must be between 0 and 10");
             }
-            
+
             scanner.nextLine();
-            System.out.println("Number of attempts left = " + (counter-1));
+            System.out.println("Number of attempts left = " + (counter - 1));
             counter--;
         }
 
