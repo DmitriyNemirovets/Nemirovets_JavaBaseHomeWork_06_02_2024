a10bfa8078be9b370cb5713b488ca6f34e53a5ab
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-03-16 19:18:42 +0200

    add files Homework13

---
+++ src/main/java/ua/hillel/nemirovets/homework/homework13/Burger.java
@@ -0,0 +1,62 @@
+package ua.hillel.nemirovets.homework.homework13;
+
+public class Burger {
+
+    String roll;
+    String meat;
+    String cheese;
+    String green;
+    String mayonnaise;
+
+    Burger(String roll, String meat, String cheese, String green, String mayonnaise) {
+        this.roll = roll;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.green = green;
+        this.mayonnaise = mayonnaise;
+    }
+
+    void burgerRegular() {
+        System.out.println("BURGER REGULAR : ");
+        System.out.println(" Roll : " + roll);
+        System.out.println(" Meat : " + meat);
+        System.out.println(" Cheese : " + cheese);
+        System.out.println(" Green : " + green);
+        System.out.println(" Mayonnaise : " + mayonnaise);
+    }
+
+    Burger(String roll, String meat, String cheese, String green) {
+        this.roll = roll;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.green = green;
+    }
+
+    void burgerDiet() {
+        System.out.println("BURGER DIET : ");
+        System.out.println(" Roll : " + roll);
+        System.out.println(" Meat : " + meat);
+        System.out.println(" Cheese : " + cheese);
+        System.out.println(" Green : " + green);
+    }
+
+    Burger(String roll, String meat, String cheese, String green, String mayonnaise, boolean doubleMeat) {
+        this.roll = roll;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.green = green;
+        this.mayonnaise = mayonnaise;
+        if (doubleMeat) {
+            this.meat += "(double)";
+        }
+    }
+
+    void burgerDoubleMeat() {
+        System.out.println("BURGER REGULAR : ");
+        System.out.println(" Roll : " + roll);
+        System.out.println(" Meat : " + meat);
+        System.out.println(" Cheese : " + cheese);
+        System.out.println(" Green : " + green);
+        System.out.println(" Mayonnaise : " + mayonnaise);
+    }
+}
