3a45e5802695844feb6c83404a9a129857f41434
Nemirovets Dmutro <dimanemirovets87@gmail.com>
2024-03-31 21:05:36 +0300

    add files Homework 16

--- src/main/java/ua/hillel/nemirovets/homework/homework16/Main.java
+++ src/main/java/ua/hillel/nemirovets/homework/homework16/Main.java
@@ -2,7 +2,7 @@ package ua.hillel.nemirovets.homework.homework16;
 
 public class Main {
     public static void main(String[] args) {
-        // Створення екземпляра Androids
+
         Androids androidPhone = new Androids();
         androidPhone.call("123456789");
         androidPhone.sms("123456789", "Hello there!");
@@ -10,7 +10,7 @@ public class Main {
         androidPhone.runTerminal();
         androidPhone.executeCommand("ls");
 
-        // Створення екземпляра iPhones
+
         iPhones iPhone = new iPhones();
         iPhone.call("987654321");
         iPhone.sms("987654321", "Hi!");
